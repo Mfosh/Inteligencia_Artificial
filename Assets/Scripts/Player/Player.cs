7140ab1f0548092b64d5bb3ca0382117b09e90c9
Mfischer <57914018+Mfosh@users.noreply.github.com>
2024-04-29 12:14:45 -0300

    Level1

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -18,26 +18,17 @@ public class Player : MonoBehaviour
         _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         _currentLife = MaxLife;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
     }
     
-    //public void Move(Vector3 Dir, Vector2 mousePos)
-    //{
-    //    _rb.velocity = Dir * _movementSpeed;
-
-   //     Vector2 lookMouse = mousePos - _rb.position;
-   //     float angle = Mathf.Atan2(lookMouse.y, lookMouse.x) * Mathf.Rad2Deg - 90f;
-   //     _rb.rotation = angle;
-   // }
     
     public void Move(Vector3 Dir)
     {
